54eb017e849246596e9113c61ba90756e31a8454
dltjdnjs <1128sam@gmail.com>
2022-12-28 12:17:35 +0900

    221228 과제

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -1,16 +1,34 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.SellerBO;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
 public class Lesson04Quiz01Controller {
+	@Autowired
+	private SellerBO sellerBO;
 
 	// http://localhost:8080/lesson04/quiz01/add_seller_view
-	@RequestMapping(path="/add_user_view", method=RequestMethod.GET)
+	@RequestMapping(path="/add_seller_view", method=RequestMethod.GET)
 	public String quiz01_1() {
 		return "lesson04/addSeller"; // addSeller.jsp로
+	}
+	
+	@PostMapping("/after_add_seller")
+	public String afterAddSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam("profileUrl") String profileUrl,
+			@RequestParam(value="temperature", required=false) Double temperature
+			) {
+		// DB insert
+		sellerBO.addSeller(nickname, profileUrl, temperature);
+		return "lesson04/afterAddSeller";
 	}
 }
--- src/main/java/com/quiz/lesson04/model/Seller.java
+++ src/main/java/com/quiz/lesson04/model/Seller.java
@@ -1,5 +1,52 @@
 package com.quiz.lesson04.model;
 
+import java.util.Date;
+
 public class Seller {
 
+	private String nickname;
+	private String profileUrl;
+	private Double temperature;
+	private Date createdAt;
+	private Date updatedAt;
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+
+	public Double getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(Double temperature) {
+		this.temperature = temperature;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 }
